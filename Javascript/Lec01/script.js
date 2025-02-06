6ac3a6ff87e5bc7c32c551e9ac99498c6e13822c
The-Parthib <rijupanja81@gmail.com>
2025-02-06 17:26:32 +0530

    initial js on html

---
+++ Javascript/Lec01/script.js
@@ -0,0 +1,27 @@
+// let Obj = {
+//     "name" : "parthib",
+//     "job code" : 5600
+// }
+
+// console.log(Obj)
+
+// Obj.salary = "500crores"
+// console.log(Obj)
+
+// +++++++++++++++++++++++++++++
+
+let age = prompt("Enter Your Age : ")
+
+if (age>18) {
+    console.log("You can drive")
+}
+else console.log("You can not drive")
+
+let a = 3;
+let b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
---
+++ Javascript/Loops/Loops.js
@@ -0,0 +1,17 @@
+// for (const c of "Parthib") {
+//     console.log(c)
+// }
+
+//++++++++++++++++++====+++++++++++++++++++
+
+// let i = 1;
+// while (i < 60000) {
+//   console.log(i);
+//   i++;
+// }
+
+let i = 0;
+do {
+  console.log("Hello Parthib, How are you..? \n");
+//   i++;
+} while (i > 1);
