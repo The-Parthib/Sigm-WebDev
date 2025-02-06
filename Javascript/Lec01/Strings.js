ccc40993f684ec965295e9821551ae20d32d5047
The-Parthib <rijupanja81@gmail.com>
2025-02-06 22:00:52 +0530

    Add string manipulation examples to Strings.js and update index.html to include FalseCalculator.js

---
+++ Javascript/Lec01/Strings.js
@@ -0,0 +1,23 @@
+// let a = "Parthib";
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+// console.log(a[5]);
+// console.log(a[6]);
+// console.log(a.length)
+//++++++++++++++__________+++++++++++++
+
+// let real_name = "Parthib";
+// let surname = "Panja"
+// console.log(`My name is ${real_name} ${surname}`);
+
+console.log("Adam D\' Alberto")
+
+let b = "Rupsa baby";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase())
+console.log(b.slice(1,3));
+console.log(b.replace('baby','bhau'))
+console.log(b.startsWith('Ru'))
