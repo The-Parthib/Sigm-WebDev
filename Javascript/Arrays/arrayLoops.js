1873cb1916190660dd3d7e9e31d0957bc6a28c0c
The-Parthib <rijupanja81@gmail.com>
2025-02-09 22:46:45 +0530

    new add

--- Javascript/Arrays/arrayLoops.js
+++ Javascript/Arrays/arrayLoops.js
@@ -1,6 +1,27 @@
-let arr = [1,43,6,53,76,32]
+let arr = [1, 43, 6, 53, 76, 32];
 
-for (let index = 0; index < arr.length; index++) {
-    const element = arr[index];
-    console.log(element)
+// for (let index = 0; index < arr.length; index++) {
+//   const element = arr[index];
+//   console.log(element);
+// }
+
+
+arr.forEach((value , index , array) => {
+    console.log(value,index,array)
+});
+
+let obj = {
+    a:1,
+    b:2,
+    c:3
+}
+for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+        // console.log(key,element)
+    }
+}
+
+for (const element of arr) {
+    console.log(element,"-->arr")
 }
