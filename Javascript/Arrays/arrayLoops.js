622c7de2003d8ae8a03eeca5d118e4daf202ba6a
The-Parthib <rijupanja81@gmail.com>
2025-02-09 22:59:07 +0530

    Update arrayLoops.js to modify array values and add squared values output

--- Javascript/Arrays/arrayLoops.js
+++ Javascript/Arrays/arrayLoops.js
@@ -1,4 +1,4 @@
-let arr = [1, 43, 6, 53, 76, 32];
+let arr = [1, 43, 6, 53, 76, 11, 12];
 
 // for (let index = 0; index < arr.length; index++) {
 //   const element = arr[index];
@@ -6,22 +6,30 @@ let arr = [1, 43, 6, 53, 76, 32];
 // }
 
 
-arr.forEach((value , index , array) => {
-    console.log(value,index,array)
-});
+// arr.forEach((value , index , array) => {
+//     console.log(value,index,array)
+// });
 
-let obj = {
-    a:1,
-    b:2,
-    c:3
-}
-for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        const element = obj[key];
-        // console.log(key,element)
-    }
-}
+// let obj = {
+//     a:1,
+//     b:2,
+//     c:3
+// }
+// for (const key in obj) {
+//     if (Object.prototype.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         // console.log(key,element)
+//     }
+// }
 
-for (const element of arr) {
-    console.log(element,"-->arr")
+// for (const element of arr) {
+//     console.log(element,"-->arr")
+// }
+
+let newArr = [];
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    newArr.push(element ** 2)
 }
+console.log(arr);
+console.log(newArr);
