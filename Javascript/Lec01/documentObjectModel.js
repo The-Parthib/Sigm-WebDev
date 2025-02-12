dda87cbf108ad8692917e2bedf05fd137d865a18
The-Parthib <rijupanja81@gmail.com>
2025-02-12 22:45:39 +0530

    Add HTML and JavaScript files for DOM manipulation; refactor factorial function to use recursion

---
+++ Javascript/Lec01/documentObjectModel.js
@@ -0,0 +1,7 @@
+document.title = "JavaScript DOM by The_Parthib";
+
+console.log(document.title);
+console.log(document.body);
+document.body.style.backgroundColor = '#813131' //applied if no css is included
+
+
--- Javascript/Lec01/factorial.js
+++ Javascript/Lec01/factorial.js
@@ -1,12 +1,17 @@
-let fact = (e) => {
-  let result = 1;
-  for (let index = e; index > 0; index--) {
-    result = index * result;
-  }
-  return result;
-};
-
-console.log(fact(5));
-
+// let fact = (e) => {
+//   let result = 1;
+//   for (let index = e; index > 0; index--) {
+//     result = index * result;
+//   }
+//   return result;
+// };
 
 // reccursion function
+let fact = (e) => {
+  if (e == 0 || e == 1) {
+    return 1;
+  }
+  return e * fact(e - 1);
+}
+
+console.log(fact(5));
