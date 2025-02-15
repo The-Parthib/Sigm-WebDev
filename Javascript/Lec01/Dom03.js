17bdf72bab59543074015da5535852c933a47a13
The-Parthib <rijupanja81@gmail.com>
2025-02-15 22:44:56 +0530

    Add new HTML and JavaScript files for DOM manipulation; update existing JavaScript with comments and examples

--- Javascript/Lec01/Dom03.js
+++ Javascript/Lec01/Dom03.js
@@ -12,6 +12,17 @@ box.style.backgroundColor = "blue";
 console.log(document.querySelectorAll(".box"));
 
 // document.querySelector(".box").style.backgroundColor = "yellow";
-document.querySelectorAll(".box").forEach((e) => {
-  e.style.backgroundColor = "pink";
-});
+
+//queryselector all returns a a list;
+
+// document.querySelectorAll(".box").forEach((e) => {  
+//   e.style.backgroundColor = "green";
+// });
+
+console.log(rups[2].matches("#redbox"));
+console.log(rups[2].matches(".box"));
+// console.log(rups[2].matches("box")) 
+// it needs # and . for ids and classes
+
+let bool = document.querySelector(".container").contains(rups[1]);
+console.log(bool);
---
+++ Javascript/Lec01/dom05.js
@@ -0,0 +1,33 @@
+/*
+document.querySelector(".box")
+<div class=​"box">​Hey I am a Box..!!​</div>​
+document.querySelector(".box").innerHTML
+'Hey I am a Box..!!'
+
+document.querySelector(".container").innerText
+'Hey I am a Box..!!'
+document.querySelector(".box").innerText
+'Hey I am a Box..!!'
+
+document.querySelector(".box").innerText = "hello how are you..?\n fine"
+'hello how are you..?\n fine'
+
+document.querySelector(".container").outerHTML
+'<div class="container">\n        <div class="box">Hey I am a Box..!!</div>\n    </div>'
+
+document.querySelector('.box')
+<div class=​"box">​Hey I am a Box..!!​</div>​
+document.querySelector('.box').hasAttribute
+ƒ hasAttribute() { [native code] }
+document.querySelector('.box').hasAttribute('style')
+false
+document.querySelector('.box2').hasAttribute('style')
+true
+document.querySelector('.box2').getAttribute
+ƒ getAttribute() { [native code] }
+document.querySelector('.box2').getAttribute('style')
+'display: flex;'
+
+*/
+
+// document.querySelector('.box2').setAttribute('style','display:none')
