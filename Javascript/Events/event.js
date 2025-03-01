207d0cccd118b3a67035eb1cce97d6b21a763729
The-Parthib <rijupanja81@gmail.com>
2025-03-01 22:17:55 +0530

    Add event listeners for button click and keydown; create new HTML files for clock and event bubbling examples

--- Javascript/Events/event.js
+++ Javascript/Events/event.js
@@ -1,8 +1,13 @@
 // let button = document.getElementById("btn");
 let button = document.querySelector("#btn");
 
-button.addEventListener('keydown',(e)=>{
+button.addEventListener('click',(e)=>{
     document.querySelector(".box").innerHTML = "<b> Button Clicked </b> <br> Enjoy your click"
     button.style.backgroundColor = 'yellow'
     console.log(e)
+})
+button.addEventListener('keydown',(e)=>{
+    document.querySelector(".box").innerHTML = "<b> Button Clicked </b> <br> Enjoy your click"
+    button.style.backgroundColor = 'yellow'
+    console.log(e.key,e.keyCode)
 })
