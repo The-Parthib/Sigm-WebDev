02768dafd0058fe8f66c2f4512ee29804654a204
The-Parthib <rijupanja81@gmail.com>
2025-03-03 22:41:04 +0530

    Add asynchronous pizza preparation example with callback functions in advJS.js and update async.html

--- Javascript/Events/advJS.js
+++ Javascript/Events/advJS.js
@@ -1,3 +1,4 @@
+/*
 console.log("Inside the Advanced JS File...!!")
 
 const a = [1, 6, 4, 3, 2, -4, 0, -1, -12, 7, 34, 12, -8];
@@ -9,3 +10,36 @@ const findNeg = (num) => {
 
 const result = a.find(findNeg) //findIndex()
 console.log(result);
+*/
+
+function getCheese(callback) {
+  setTimeout(() => {
+    let cheese = "🧀";
+    console.log("Here is the cheese", cheese);
+    callback(cheese);
+  }, 2000);
+}
+
+function getDough(cheese,callback) {
+  setTimeout(() => {
+    let dough = cheese + "🫓";
+    console.log("Here is the dough", dough);
+    callback(dough);
+  }, 2000);
+}
+
+function getPizza(dough,callback) {
+  setTimeout(() => {
+    let pizza = dough + "🍕";
+    console.log("Here is the Pizza", pizza);
+    callback(pizza);
+  }, 2000);
+}
+
+getCheese((cheese) => {
+  getDough(cheese,(dough)=>{
+    getPizza(dough,(pizza)=>{
+        console.log("Your Hot Pizza is Ready ",pizza)
+    })
+  })
+});
