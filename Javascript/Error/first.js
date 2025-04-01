dde547e27c66390acb500539b23a3ae70109450b
The-Parthib <rijupanja81@gmail.com>
2025-04-01 23:11:14 +0530

    Add initial HTML and JavaScript files for error handling example

---
+++ Javascript/Error/first.js
@@ -0,0 +1,5 @@
+let a = parseInt(prompt("Enter a number"));
+let b = parseInt(prompt("Enter another number"));
+let sum = a + b;
+alert("The sum is: " + sum);
+document.body.querySelector("h1").innerText = "The sum is: " + sum;
